3b569a00a653f641d8f36c50283300f1bb9a8da0
Lidia Kovac <lidia.kovac1998@gmail.com>
2021-12-18 13:11:47 +0100

    adds back react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 
 import './App.css';
 import Landing from "./pages/Landing"
+import {BrowserRouter, Route, Routes} from "react-router-dom"
 const App: React.FC = () => {
   return (
-    <Landing/>
+    <BrowserRouter>
+    <Routes>
+
+      <Route path='/' element={<Landing/>} />
+    </Routes>
+    </BrowserRouter>
   );
 }
 
